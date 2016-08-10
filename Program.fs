0447ce99beefd9d0ff881b6d6b7f0f6806dc7294
Daniel Markham <DanielBMarkham@Hotmail.com>
2016-08-10 03:05:09 -0400

    On remembers Id and State

--- Program.fs
+++ Program.fs
@@ -3,24 +3,30 @@
         then ()
         else failwith msg
 
-type Light =
+// Now lights have numbers
+type LightState =
     LIGHTOFF
     | LIGHTON
-
+type Light = 
+    {
+        LightID:int;
+        LightState:LightState
+    }
 type LightCommand =
     TURNON
     | TURNOFF    
 
 let updateLight light lightCommand =
     match lightCommand with
-        | TURNON->LIGHTON
-        | TURNOFF->LIGHTOFF
+        | TURNON->{light with LightState=LIGHTON}
+        | TURNOFF->{light with LightState=LIGHTOFF}
 
 [<EntryPoint>]
 let main argv = 
-    // The function has changed. This test no longer valid
-    // ASSERT (updateLight LIGHTOFF = LIGHTOFF) "Lights are not changed at initialization"
-    ASSERT (updateLight LIGHTOFF TURNON = LIGHTON) "Lights that are off can be turned on"
-    ASSERT (updateLight LIGHTON TURNOFF = LIGHTOFF) "Lights that are on can be turned off"
-
+    // now the function has changed again and the tests are no longer valid
+    //ASSERT (updateLight LIGHTOFF TURNON = LIGHTON) "Lights that are off can be turned on"
+    //ASSERT (updateLight LIGHTON TURNOFF = LIGHTOFF) "Lights that are on can be turned off"
+    let myLight = {LightID=10;LightState=LIGHTOFF;}
+    ASSERT ( (updateLight myLight TURNON).LightID = 10) "On remembers id"
+    ASSERT ( (updateLight myLight TURNON).LightState = LIGHTON) "On remembers state"
     0 // return an integer exit code
