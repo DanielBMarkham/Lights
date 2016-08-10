74519bda95003e78082f36a73bd36b3d2366fca4
Daniel Markham <DanielBMarkham@Hotmail.com>
2016-08-10 03:41:45 -0400

    Not lights controlled at the wrong time

--- Program.fs
+++ Program.fs
@@ -12,21 +12,40 @@ type Light =
     }
 type LightCommand =
     TURNON
-    | TURNOFF    
+    | TURNOFF
+type Day = EVERYDAY | SUNDAY | MONDAY | TUESDAY | WEDNESDAY | THURSDAY | FRIDAY | SATURDAY
+type CommandTime =
+    {
+        Day:Day;
+        Minute:int;
+    }
 let updateLight light lightCommand =
     match lightCommand with
         | TURNON->{light with LightState=LIGHTON}
         | TURNOFF->{light with LightState=LIGHTOFF}
-// now we have an updateTick, although it does nothing
-// note that due to the language, I have to return something
-let updateTick =
-    let recordsProcessed = None;
-    recordsProcessed
+type LightSchedule =
+    {
+        Light:Light;
+        LightCommand:LightCommand;
+        CommandTime:CommandTime;
+    }
+let updateTick schedule light time =
+    // I update the light if it is the right time.
+    // Note that pattern matching forces me to exercise all the paths here
+    let updatedLight =
+        match schedule.CommandTime.Day with
+            | EVERYDAY->
+                if schedule.CommandTime.Minute = time.Minute then updateLight schedule.Light schedule.LightCommand else light
+            |_ -> if (schedule.CommandTime.Day = time.Day) && (schedule.CommandTime.Minute = time.Minute) then updateLight schedule.Light schedule.LightCommand else light
+    updatedLight
 
 [<EntryPoint>]
 let main argv = 
     let myLight = {LightID=10;LightState=LIGHTOFF;}
+    let mySchedule = {Light = myLight; LightCommand=TURNON; CommandTime={Day=EVERYDAY;Minute=1200;};}
+    // Note that because of immutability, myLight doesn't change. Each function returns a new light
     ASSERT ( (updateLight myLight TURNON).LightID = 10) "On remembers id"
     ASSERT ( (updateLight myLight TURNON).LightState = LIGHTON) "On remembers state"
-    ASSERT (updateTick.IsNone) "No Lights Controlled During Initialization"
+    let myTestTime = {Day=SUNDAY;Minute=1199;}
+    ASSERT ((updateTick mySchedule myLight myTestTime).LightState=LIGHTOFF)  "No Lights Controlled at the wrong time"
     0 // return an integer exit code
