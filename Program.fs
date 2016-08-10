02fd8f096d60cfe556b718bdfaccac188d4787b7
Daniel Markham <DanielBMarkham@Hotmail.com>
2016-08-10 02:50:48 -0400

    Lights can be toggled

--- Program.fs
+++ Program.fs
@@ -4,13 +4,23 @@
         else failwith msg
 
 type Light =
-        LIGHTOFF
-        | LIGHTON
+    LIGHTOFF
+    | LIGHTON
 
-let updateLight light =
-    light
+type LightCommand =
+    TURNON
+    | TURNOFF    
+
+let updateLight light lightCommand =
+    match lightCommand with
+        | TURNON->LIGHTON
+        | TURNOFF->LIGHTOFF
 
 [<EntryPoint>]
 let main argv = 
-    ASSERT (updateLight LIGHTOFF = LIGHTOFF) "Lights are not changed at initialization"
+    // The function has changed. This test no longer valid
+    // ASSERT (updateLight LIGHTOFF = LIGHTOFF) "Lights are not changed at initialization"
+    ASSERT (updateLight LIGHTOFF TURNON = LIGHTON) "Lights that are off can be turned on"
+    ASSERT (updateLight LIGHTON TURNOFF = LIGHTOFF) "Lights that are on can be turned off"
+
     0 // return an integer exit code
